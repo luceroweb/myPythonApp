7ad636706f0ca31f1df1f3843ab9032d4340013c
Juan Lucero <luceroweb@gmail.com>
2023-08-22 12:24:17 -0700

    chore(debug): add django-debug-toolbar package

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("__debug__", include("debug_toolbar.urls")),
 ]
